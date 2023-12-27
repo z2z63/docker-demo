09a1a7a86e1b25efc8cf0e1db776b3444da4089d
z2z63 <vituralfuture@gmail.com>
2023-12-27 16:52:08 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd BookManager2 || exit
+mvn package
+cd ..
+
+cd BookManagerVue || exit
+npm run build:prod
+cd ..
+
+docker build -f Dockerfile.backend . -t backend
+docker build -f Dockerfile.frontend . -t frontend
+docker build -f Dockerfile.database . -t database
+docker build -f Dockerfile.myredis . -t myredis
+
+docker compose up -d
+
